f1bd6c0ddc14a7409919927b01bce7495a5229b1
Sakshi Gupta <Sakshi221310@gmail.com>
2024-08-24 19:04:43 +0530

    Add files via upload

---
+++ Swapingusingthirdvariable.java
@@ -0,0 +1,13 @@
+public class Swapingusingthirdvariable{
+    public static void main(String[] args) {
+        int x=10;
+        int y=20;
+        int z;
+        System.out.println("Before swaping numbers:"+x +y);
+        z=y;
+        y=x;
+        x=z;
+    
+System.out.println("After swaping numbers:"+x +y);
+    }
+}
