cad0d998cbe77d14fc99e1e6620f2fdd04abb73c
Sakshi Gupta <Sakshi221310@gmail.com>
2024-08-25 15:36:16 +0530

    Add files via upload

---
+++ fahrenheittocelcius.java
@@ -0,0 +1,11 @@
+public class fahrenheittocelcius {
+ 
+        public static void main(String[] args) {
+            float f=300;
+            float  c=(f-32)*5/9;
+            
+            System.out.println("The temprature in="+c);
+        }
+        
+    
+}
