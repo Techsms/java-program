c13c40cbf9423f881f876c0d73eb45156383939b
Sakshi Gupta <Sakshi221310@gmail.com>
2024-08-28 14:42:43 +0530

    Add files via upload

---
+++ LargerAmongthree.java
@@ -0,0 +1,19 @@
+public class LargerAmongthree {
+    public static void main(String[] args) {
+        short a,b,c;
+        a=25;
+        b=40;
+        c=100;
+if (a>b && a>c)
+System.out.println("Larger="+a);
+else if (b>a && b>c)
+System.out.println("Larger="+b);
+else 
+System.out.println("Larger="+c);
+
+
+
+    
+    }
+    
+}
